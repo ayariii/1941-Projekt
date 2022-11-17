fd4e181fe3048c607c2d1fc851dadf3c1b077eef
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2022-11-17 12:16:30 +0100

    Add files via upload

---
+++ Assets/Shooting_Script.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting_Script : MonoBehaviour
+{
+    public Rigidbody projectile;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Rigidbody clone;
+            clone = Instantiate(projectile, new Vector3(0, 0, 4f), transform.rotation);
+            clone.velocity = new Vector3(0, 0, 3.4f);
+        }
+    }
+}
+
